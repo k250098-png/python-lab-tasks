24e7c63b6cbde9278adc871f2e68a083987ea50d
Hasan Akram <k250098@nu.edu.pk>
2026-08-26 23:48:56 +0500

    Update q4.py

--- q4.py
+++ q4.py
@@ -3,12 +3,9 @@ total_elements = int(input("How many numbers do you want to enter? "))
 
 numbers = []
 
-
 for i in range(total_elements):
-    num = float(input(f"Enter number {i + 1}: "))
+    num = float(input("Enter number " + str(i + 1) + ": "))
     numbers.append(num)
-
-
 total_sum = sum(numbers)
 
 print("Sum of all elements:", total_sum)
