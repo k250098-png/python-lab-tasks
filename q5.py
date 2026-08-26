fb1c8cecf50bcdec22d8aa2c1df800dae4ea7e59
Hasan Akram <k250098@nu.edu.pk>
2026-08-26 23:50:13 +0500

    Update q5.py

--- q5.py
+++ q5.py
@@ -3,12 +3,10 @@ total_elements = int(input("How many numbers do you want in the list? "))
 
 numbers = []
 for i in range(total_elements):
-    num = float(input(f"Enter number {i + 1}: "))
+    num = float(input("Enter number " + str(i + 1) + ": "))
     numbers.append(num)
 
-
 threshold = float(input("Enter the target number: "))
-
 
 filtered_list = []
 for num in numbers:
